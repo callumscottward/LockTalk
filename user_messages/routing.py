c1565f899ac9cc47fdc43b469b0de4e2240dd747
CamWri <camdenawright2022@gmail.com>
2026-03-23 19:25:35 -0500

    Small bug fix regarding naming convention

--- user_messages/routing.py
+++ user_messages/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 websocket_urlpatterns = [
     # The path name
     re_path(r'ws/conversation/(?P<conversation_id>[0-9a-f-]+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'ws/conversations/$', consumers.ConversationsConsumer.as_asgi()),
+    re_path(r'ws/conversations/$', consumers.ConversationConsumer.as_asgi()),
 ]

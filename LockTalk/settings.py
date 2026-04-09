5fd4055bea67e3cd728c99f9d048b0a21a0ab5d5
callumscottward <callumward@unomaha.edu>
2026-04-09 10:10:04 -0500

    Allow all hosts again to support EC2 instance IP reassignment

--- LockTalk/settings.py
+++ LockTalk/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "100.31.131.162"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

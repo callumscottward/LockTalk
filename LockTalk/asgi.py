4f80e49cd929939589feace28630bb271f421e5d
callumscottward <callumward@unomaha.edu>
2026-04-09 00:03:48 -0500

    Update ASGI configuration for hosting on daphne

--- LockTalk/asgi.py
+++ LockTalk/asgi.py
@@ -8,18 +8,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import user_messages.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            user_messages.routing.websocket_urlpatterns
-        )
+        URLRouter(user_messages.routing.websocket_urlpatterns)
     ),
 })

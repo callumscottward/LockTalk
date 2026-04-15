544efa7474624ccb8ab19c56f4fcf1f00c861c50
callumscottward <callumward@unomaha.edu>
2026-04-15 18:05:09 -0500

    Change default page to Dashboard and redirect to login if unauthenticated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import UserProfile from './pages/UserProfile';
 import Login from './pages/Login';
@@ -7,19 +7,23 @@ import UserManagement from './pages/UserManagement';
 import Logs from './pages/Logs';
 import Signup from './pages/Signup';
 import ChatDirectory from './pages/ChatDirectory';
+import ProtectedRoutes from './ProtectedRoutes';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/userProfile" element={<UserProfile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/userManagement" element={<UserManagement />} />
-        <Route path="/logs" element={<Logs />} />
-        <Route path="/chatDirectory" element={<ChatDirectory />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/userProfile" element={<UserProfile />} />
+          <Route path="/userManagement" element={<UserManagement />} />
+          <Route path="/logs" element={<Logs />} />
+          <Route path="/chatDirectory" element={<ChatDirectory />} />
+        </Route>
       </Routes>
     </div>
   )

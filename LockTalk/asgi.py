5886fdb22467af17315f4ae3372227554823389d
callumscottward <callumward@unomaha.edu>
2026-04-29 00:54:19 -0500

    Fix asgi.py error with daphne

--- LockTalk/asgi.py
+++ LockTalk/asgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import user_messages.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),

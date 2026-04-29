78bd5c0a8b1268cec1884412fcc443977b030b81
callumscottward <callumward@unomaha.edu>
2026-04-29 00:45:33 -0500

    Remove unnecessary changes

--- LockTalk/asgi.py
+++ LockTalk/asgi.py
@@ -8,20 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
-
 from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import user_messages.routing
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LockTalk.settings')
+
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(user_messages.routing.websocket_urlpatterns)
+        URLRouter(
+            user_messages.routing.websocket_urlpatterns
+        )
     ),
 })

cddf9df3dddb2a9c90a2149bc12b3da34ef9470d
bfarrelluno <151088136+bfarrelluno@users.noreply.github.com>
2026-04-30 21:19:58 -0500

    Update test_consumers.py

--- user_messages/test_consumers.py
+++ user_messages/test_consumers.py
@@ -2,6 +2,7 @@ import json
 from channels.testing import WebsocketCommunicator
 from django.test import TransactionTestCase
 from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
 from user_messages.models import Conversation, Message
 from LockTalk.asgi import application
 
@@ -10,15 +11,21 @@ User = get_user_model()
 
 class ChatConsumerTest(TransactionTestCase):
 
-    async def test_connect_success(self):
-
-        user = User.objects.create_user(
+    async def create_user(self):
+        return await database_sync_to_async(User.objects.create_user)(
             username="testuser",
             password="password"
         )
 
-        conversation = Conversation.objects.create()
-        conversation.participants.add(user)
+    async def create_conversation(self, user):
+        convo = await database_sync_to_async(Conversation.objects.create)()
+        await database_sync_to_async(convo.participants.add)(user)
+        return convo
+
+    async def test_connect_success(self):
+
+        user = await self.create_user()
+        conversation = await self.create_conversation(user)
 
         communicator = WebsocketCommunicator(
             application,
@@ -34,13 +41,8 @@ class ChatConsumerTest(TransactionTestCase):
 
     async def test_send_message(self):
 
-        user = User.objects.create_user(
-            username="testuser",
-            password="password"
-        )
-
-        conversation = Conversation.objects.create()
-        conversation.participants.add(user)
+        user = await self.create_user()
+        conversation = await self.create_conversation(user)
 
         communicator = WebsocketCommunicator(
             application,
